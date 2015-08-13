9ef02f920744de1841c993207bb4aa4dd36548e8
Rachel Warren <raelwa@gmail.com>
2015-08-13 18:15:44 -0400

    config file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,8 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.serve_static_assets = true
-  config.assets.compile = true
-  
+  config.serve_static_assets = false
+  config.assets.compile = false
+  config.assets.digest = true
+
 end
